32c658155f13342ff9b45451f432bc03e626f6cf
Adria Huertas <adriahc@gmail.com>
2023-09-07 13:35:34 +0200

    Changed api address

--- src/services/questions.ts
+++ src/services/questions.ts
@@ -1,5 +1,5 @@
 export const getAllQuestions = async (limit: number = 10) => {
-  return await fetch('http://localhost:5173/data.json')
+  return await fetch('./data.json')
     .then(async (res) => await res.json())
     .then((data) => data.sort(() => Math.random() - 0.5).slice(0, limit))
 }
